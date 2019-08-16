64cc19964a73732d58cc5cb1cda900a53f1c2af5
akrahenbuhl <akrahenbuhl@instructure.com>
2019-08-16 15:39:19 -0600

    Remove dock icon. Add initial event monitor, which doesn't work

--- NectieApp/AppDelegate.swift
+++ NectieApp/AppDelegate.swift
@@ -11,14 +11,47 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    var eventMonitor: EventMonitor?
 
-
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off..."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) - \(quoteAuthor)")
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Necktie", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("Necktie"))
+            button.action = #selector(printQuote(_:))
+        }
+        constructMenu()
+        
+        // Init an event monitor
+        eventMonitor = EventMonitor(mask: [.keyDown]) { [weak self]
+        event in
+            if let strongSelf = self {
+                strongSelf.printQuote(event)
+            }
+        }        
+        eventMonitor?.start()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        if eventMonitor != nil {
+            eventMonitor?.stop()
+        }
     }
 
 
