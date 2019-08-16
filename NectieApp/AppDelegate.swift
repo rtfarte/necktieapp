f6d95bceeb984d0b89c6976bcec0bcad79c42d9a
akrahenbuhl <akrahenbuhl@instructure.com>
2019-08-16 14:32:38 -0600

    initial commit

---
+++ NectieApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NectieApp
+//
+//  Created by Art Krahenbuhl on 8/16/19.
+//  Copyright © 2019 necktieapp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NectieApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NectieApp
+//
+//  Created by Art Krahenbuhl on 8/16/19.
+//  Copyright © 2019 necktieapp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
